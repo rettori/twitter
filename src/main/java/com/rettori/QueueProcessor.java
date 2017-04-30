3b7331dbab6f16f087bd5059337ee4dbe7676a6d
Diogenes Rettori <rettori@gmail.com>
2017-04-30 00:04:55 -0400

    ok

--- src/main/java/com/rettori/QueueProcessor.java
+++ src/main/java/com/rettori/QueueProcessor.java
@@ -8,5 +8,7 @@ public class QueueProcessor implements Processor {
 	public void process(Exchange inExchange) {
 		String queueName = MyConnectionFactory.readFile("/data/destination-address");
 		inExchange.getContext().getProperties().put("queueName", queueName);
+		String body = inExchange.getIn().getBody(String.class);
+		inExchange.getIn().setBody(body);
 	}
 }
